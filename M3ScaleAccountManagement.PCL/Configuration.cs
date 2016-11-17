28e5c3d59cae7626112e138ab0519660e1765df1
Waseem Hassan <Waseem Hassan>
2016-11-17 14:45:40 +0500

    Added

---
+++ M3ScaleAccountManagement.PCL/Configuration.cs
@@ -0,0 +1,10 @@
+
+namespace M3ScaleAccountManagement.PCL
+{
+    public partial class Configuration
+    {
+        //The base Uri for API calls
+        public static string BaseUri = "http://example.com";
+
+    }
+}
---
+++ M3ScaleAccountManagement.PCL/Http/Request/HttpMethod.cs
@@ -0,0 +1,11 @@
+namespace M3ScaleAccountManagement.PCL.Http.Request
+{
+    public enum HttpMethod
+    {
+        GET,
+        POST,
+        PUT,
+        PATCH,
+        DELETE
+    }
+}
